3031962f0b73054d676646078fa1f8edf33dd18f
AD\daniel <daniel@cpttm.org.mo>
2023-11-18 01:12:51 +0800

    show msg on login failed

--- app/src/main/java/com/example/a04_module_c/MainActivity.kt
+++ app/src/main/java/com/example/a04_module_c/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -52,11 +53,28 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onResponse(call: Call, response: Response) {
                     println(response)
-                    if (!response.isSuccessful) {
-                        Toast.makeText(this@MainActivity, "login failed", Toast.LENGTH_LONG).show()
+                    if (response.isSuccessful) {
+                        // 將 response 的 body 解析出來
+                        val jsonData: String = response.body?.string().toString()
+                        val resObj = JSONObject(jsonData)
+                        val validated = resObj.getBoolean("validated")
+
+                        // 查看解析結果中的 validated 是 true / false， false 表示賬號密碼錯誤
+                        if (validated) {
+                            val workPage = Intent(this@MainActivity, WorkPage::class.java)
+                            startActivity(workPage)
+                        } else {
+                            // onResponse 裡不能直接使用 Toast，必須使用此方法調用 UI Thread 來執行
+                            runOnUiThread {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "name or password incorrect",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
                     } else {
-                        val workPage = Intent(this@MainActivity, WorkPage::class.java)
-                        startActivity(workPage)
+                        println("連不上 server 才會出現在這裡，這時候就要檢查 server 返回的 response status 是否 200 OK， 或者連不上 api server")
                     }
                 }
             })
